e44eebaac743755095f3920bbf29348f26800cd1
valentinGailliez <63590566+valentinGailliez@users.noreply.github.com>
2022-02-11 10:07:59 +0100

    Création d'une fenetre

---
+++ app/src/main/java/View/Window.java
@@ -0,0 +1,60 @@
+package View;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Window extends Application{
+
+	public Window() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		Button button = new Button();
+		button.setText("Open a New Window");
+
+		button.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+
+				Label secondLabel = new Label("I'm a Label on new Window");
+
+				StackPane secondaryLayout = new StackPane();
+				secondaryLayout.getChildren().add(secondLabel);
+
+				Scene secondScene = new Scene(secondaryLayout, 230, 100);
+
+				// New window (Stage)
+				Stage newWindow = new Stage();
+				newWindow.setTitle("Second Stage");
+				newWindow.setScene(secondScene);
+
+				// Set position of second window, related to primary window.
+				newWindow.setX(primaryStage.getX() + 200);
+				newWindow.setY(primaryStage.getY() + 100);
+
+				newWindow.show();
+			}
+		});
+
+		StackPane root = new StackPane();
+		root.getChildren().add(button);
+
+		Scene scene = new Scene(root, 450, 250);
+
+		primaryStage.setTitle("JavaFX Open a new Window (o7planning.org)");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+}
