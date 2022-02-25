3d8edb021a4437d9e8ca883e2ea80b830cbb30f5
valentinGailliez <63590566+valentinGailliez@users.noreply.github.com>
2022-02-25 11:05:45 +0100

    Creation Tree and Modification FIgure/Reader

--- app/src/main/java/BSP/BinarySpacePartitionTree.java
+++ app/src/main/java/BSP/BinarySpacePartitionTree.java
@@ -1,8 +1,30 @@
 package BSP;
 
+import java.util.List;
+
+import Model.Figure;
+import Model.Segment;
+import ReaderFile.ReaderFile;
+
 public class BinarySpacePartitionTree {
 
-    public BinarySpacePartitionTree() {
+    Tree tree;
+    ReaderFile readerFile;
+
+    public BinarySpacePartitionTree(String path) {
+        readerFile = new ReaderFile(path);
+        Figure figure = readerFile.getFigure();
+        this.tree = ConstructingBSP(figure.getSegments());
+    }
+
+    private Tree ConstructingBSP(List<Segment> segments) {
+
+        // Check the number of segment
+        if (segments.size() <= 1) {
+            this.tree.node = segments.get(0);
+            return this.tree;
+        }
+        return null;
 
     }
 }
---
+++ app/src/main/java/BSP/Tree.java
@@ -0,0 +1,12 @@
+package BSP;
+
+import java.util.List;
+
+import Model.Segment;
+
+public class Tree {
+    Segment node;
+    Tree positivSubTree;
+    Tree negativSubTree;
+
+}
